3080304f9f442abdb81d8ce6c4f662157c1b4947
Anton Zdanov <akumael@gmail.com>
2017-12-11 16:22:30 +0200

    Add flags to svg images. (Slow)

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,5 @@
 /* eslint-disable no-param-reassign,import/no-dynamic-require,global-require */
-import countries from "./countries.json";
+import dataset from "./countries.json";
 import "../css/index.css";
 
 // Load all flags from directory
@@ -10,46 +10,15 @@ req.keys().forEach(key => {
     flags[/[^./]+(?=\.svg$)/.exec(key)[0]] = req(key);
 });
 
-const w = 500;
-const h = 300;
+const w = 1000;
+const h = 600;
 const flagWidth = 20;
 const flagHeight = 15;
-
-// Original data
-const dataset = {
-    nodes: [
-        { name: "Adam" },
-        { name: "Bob" },
-        { name: "Carrie" },
-        { name: "Donovan" },
-        { name: "Edward" },
-        { name: "Felicity" },
-        { name: "George" },
-        { name: "Hannah" },
-        { name: "Iris" },
-        { name: "Jerry" },
-    ],
-    edges: [
-        { source: 0, target: 1 },
-        { source: 0, target: 2 },
-        { source: 0, target: 3 },
-        { source: 0, target: 4 },
-        { source: 1, target: 5 },
-        { source: 2, target: 5 },
-        { source: 2, target: 5 },
-        { source: 3, target: 4 },
-        { source: 5, target: 8 },
-        { source: 5, target: 9 },
-        { source: 6, target: 7 },
-        { source: 7, target: 8 },
-        { source: 8, target: 9 },
-    ],
-};
 
 // Initialize a simple force layout, using the nodes and edges in dataset
 const force = d3
     .forceSimulation(dataset.nodes)
-    .force("charge", d3.forceManyBody())
+    .force("charge", d3.forceManyBody().distanceMax(100))
     .force("link", d3.forceLink(dataset.edges))
     .force(
         "center",
@@ -58,6 +27,11 @@ const force = d3
             .x(w / 2)
             .y(h / 2),
     );
+
+d3
+    .select("body")
+    .append("h1")
+    .text("National Contiguity");
 
 // Create SVG element
 const svg = d3
@@ -75,30 +49,6 @@ const edges = svg
     .style("stroke", "#ccc")
     .style("stroke-width", 1);
 
-// Define drag event functions
-function dragStarted(d) {
-    if (!d3.event.active) {
-        force.alphaTarget(0.3).restart();
-    }
-    d.fx = d.x;
-    d.fy = d.y;
-}
-
-function dragging(d) {
-    d.fx = d3.event.x;
-    d.fy = d3.event.y;
-}
-
-function dragEnded(d) {
-    if (!d3.event.active) {
-        force.alphaTarget(0);
-    }
-    d.fx = null;
-    d.fy = null;
-}
-
-console.table(countries);
-
 // Create nodes as circles
 const nodes = svg
     .selectAll("image")
@@ -107,17 +57,10 @@ const nodes = svg
     .append("image")
     .attr("width", flagWidth)
     .attr("height", flagHeight)
-    .attr("xlink:href", flags.ad)
-    .call(
-        d3
-            .drag() // Define what to do on drag events
-            .on("start", dragStarted)
-            .on("drag", dragging)
-            .on("end", dragEnded),
-    );
+    .attr("xlink:href", ({ code }) => flags[code]);
 
 // Add a simple tooltip
-nodes.append("title").text(d => d.name);
+nodes.append("title").text(({ country }) => country);
 
 // Every time the simulation "ticks", this will be called
 force.on("tick", () => {
