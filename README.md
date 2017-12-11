5a12a0495f4edab79453625fb0a90ff83cb22239
Anton Zdanov <akumael@gmail.com>
2017-12-11 22:39:47 +0200

    Finish. Change flags to png for speed. Add collision, boundaries, zoom.

--- README.md
+++ README.md
@@ -1,3 +1,10 @@
 # Force Graph
 
 D3.js Force Graph made for FreeCodeCamp.
+
+This graph illustrates borders and proximity of countries.
+
+## Used
+
+* [Flags](https://github.com/hjnilsson/country-flags)
+* [Data](https://raw.githubusercontent.com/DealPete/forceDirected/master/countries.json)
--- src/js/index.js
+++ src/js/index.js
@@ -1,47 +1,84 @@
-/* eslint-disable no-param-reassign,import/no-dynamic-require,global-require */
-import dataset from "./countries.json";
+/* eslint-disable no-param-reassign,import/no-dynamic-require,global-require,no-return-assign */
 import "../css/index.css";
+import dataset from "./countries.json";
 
-// Load all flags from directory
-const req = require.context("../flags", false, /.svg$/);
+const req = require.context("../assets", false, /.png$/);
 const flags = {};
 req.keys().forEach(key => {
-    // extract filename, e.g "./de.svg" => "de"
-    flags[/[^./]+(?=\.svg$)/.exec(key)[0]] = req(key);
+    // extract filename, e.g "./us.png" => "us"
+    flags[/[^./]+(?=\.png$)/.exec(key)[0]] = req(key);
 });
 
 const w = 1000;
-const h = 600;
-const flagWidth = 20;
-const flagHeight = 15;
+const h = 500;
+const flagSize = 32;
 
 // Initialize a simple force layout, using the nodes and edges in dataset
-const force = d3
+const simulation = d3
     .forceSimulation(dataset.nodes)
-    .force("charge", d3.forceManyBody().distanceMax(100))
-    .force("link", d3.forceLink(dataset.edges))
+    .force("charge", d3.forceManyBody().strength(-100))
+    .force("link", d3.forceLink(dataset.edges).distance(70))
     .force(
         "center",
         d3
             .forceCenter()
             .x(w / 2)
             .y(h / 2),
-    );
+    )
+    .force("collision", d3.forceCollide());
 
 d3
     .select("body")
     .append("h1")
     .text("National Contiguity");
 
-// Create SVG element
+d3
+    .select("body")
+    .append("p")
+    .text("This chart shows countries which have common borders or have close proximity to each other.");
+d3
+    .select("body")
+    .append("small")
+    .text("To move click and drag. To zoom use the mouse wheel.");
+
 const svg = d3
     .select("body")
+    .append("main")
     .append("svg")
     .attr("width", w)
     .attr("height", h);
 
+svg
+    .append("rect")
+    .attr("width", "100%")
+    .attr("height", "100%")
+    .attr("fill", "#ecf0f1");
+
+const chart = svg.append("g");
+
+const drag = {
+    start(d) {
+        if (!d3.event.active) {
+            simulation.alphaTarget(0.3).restart();
+        }
+        d.fx = d.x;
+        d.fy = d.y;
+    },
+    during(d) {
+        d.fx = d3.event.x;
+        d.fy = d3.event.y;
+    },
+    end(d) {
+        if (!d3.event.active) {
+            simulation.alphaTarget(0);
+        }
+        d.fx = null;
+        d.fy = null;
+    },
+};
+
 // Create edges as lines
-const edges = svg
+const edges = chart
     .selectAll("line")
     .data(dataset.edges)
     .enter()
@@ -49,26 +86,85 @@ const edges = svg
     .style("stroke", "#ccc")
     .style("stroke-width", 1);
 
-// Create nodes as circles
-const nodes = svg
+function movingTooltip(x, y) {
+    d3
+        .select("#tooltip")
+        .attr("x", x + flagSize / 2)
+        .attr("y", y);
+}
+
+function stoppingTooltip() {
+    d3.select("#tooltip").remove();
+}
+
+function startingTooltip(x, y, country) {
+    chart
+        .append("text")
+        .attr("id", "tooltip")
+        .attr("x", x + flagSize / 2)
+        .attr("y", y)
+        .attr("text-anchor", "middle")
+        .attr("font-family", "sans-serif")
+        .attr("font-size", "11px")
+        .attr("font-weight", "bold")
+        .attr("fill", "black")
+        .text(country);
+}
+
+// Create nodes as flags
+const nodes = chart
     .selectAll("image")
     .data(dataset.nodes)
     .enter()
-    .append("image")
-    .attr("width", flagWidth)
-    .attr("height", flagHeight)
-    .attr("xlink:href", ({ code }) => flags[code]);
+    .append("svg:image")
+    .attr("xlink:href", ({ code }) => flags[code])
+    .attr("width", flagSize)
+    .attr("height", flagSize)
+    .on("mouseover", ({ x, y, country }) => {
+        startingTooltip(x, y, country);
+    })
+    .on("mousemove", ({ x, y }) => {
+        movingTooltip(x, y);
+    })
+    .on("mouseout", () => {
+        stoppingTooltip();
+    })
+    .call(
+        d3
+            .drag()
+            .on("start", d => {
+                drag.start(d);
+            })
+            .on("drag", d => {
+                drag.during(d);
+                const { x, y } = d;
+                movingTooltip(x, y);
+            })
+            .on("end", d => {
+                drag.end(d);
+                stoppingTooltip();
+            }),
+    );
 
-// Add a simple tooltip
-nodes.append("title").text(({ country }) => country);
+function zoomActions() {
+    chart.attr("transform", d3.event.transform);
+}
 
-// Every time the simulation "ticks", this will be called
-force.on("tick", () => {
+const zoomHandler = d3
+    .zoom()
+    .scaleExtent([0.2, 2.0])
+    .translateExtent([[-w * 3, -h * 3], [w * 3, h * 3]])
+    .on("zoom", zoomActions);
+
+zoomHandler(svg);
+
+simulation.on("tick", () => {
+    nodes
+        .attr("x", d => (d.x = Math.max(-w * 3 + flagSize, Math.min(w * 3 - flagSize, d.x))))
+        .attr("y", d => (d.y = Math.max(-h * 3 + flagSize, Math.min(h * 3 - flagSize, d.y))));
     edges
-        .attr("x1", d => d.source.x + flagWidth / 2)
-        .attr("y1", d => d.source.y + flagHeight / 2)
-        .attr("x2", d => d.target.x + flagWidth / 2)
-        .attr("y2", d => d.target.y + flagHeight / 2);
-
-    nodes.attr("x", d => d.x).attr("y", d => d.y);
+        .attr("x1", d => d.source.x + flagSize / 2)
+        .attr("y1", d => d.source.y + flagSize / 2)
+        .attr("x2", d => d.target.x + flagSize / 2)
+        .attr("y2", d => d.target.y + flagSize / 2);
 });
