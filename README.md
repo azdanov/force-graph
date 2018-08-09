d99077f07de25b7604930da0d2e8d26d7b3d051d
Anton Zdanov <azdanov@protonmail.com>
2018-08-09 07:40:10 +0300

    feat(project): update for showcase

--- README.md
+++ README.md
@@ -10,27 +10,27 @@ This graph illustrates borders and proximity of countries.
 
 ## Tools
 
-* [d3](https://github.com/d3/d3)
-* [babel](https://github.com/babel/babel)
-* [webpack](https://github.com/webpack)
-* [eslint](https://github.com/eslint/eslint)
-* [airbnb](https://github.com/airbnb/javascript)
-* [prettier](https://github.com/prettier/prettier)
-* [stylelint](https://github.com/stylelint/stylelint)
-* [cross-env](https://github.com/kentcdodds/cross-env)
-* [gh-pages](https://github.com/tschaub/gh-pages)
-* [npm-run-all](https://github.com/mysticatea/npm-run-all)
-* [rimraf](https://github.com/isaacs/rimraf)
-* [country-flags](https://github.com/hjnilsson/country-flags)
+-   [d3](https://github.com/d3/d3)
+-   [babel](https://github.com/babel/babel)
+-   [webpack](https://github.com/webpack)
+-   [eslint](https://github.com/eslint/eslint)
+-   [airbnb](https://github.com/airbnb/javascript)
+-   [prettier](https://github.com/prettier/prettier)
+-   [stylelint](https://github.com/stylelint/stylelint)
+-   [cross-env](https://github.com/kentcdodds/cross-env)
+-   [gh-pages](https://github.com/tschaub/gh-pages)
+-   [npm-run-all](https://github.com/mysticatea/npm-run-all)
+-   [rimraf](https://github.com/isaacs/rimraf)
+-   [country-flags](https://github.com/hjnilsson/country-flags)
 
 ## Data
 
-* [countries.json](https://raw.githubusercontent.com/DealPete/forceDirected/master/countries.json)
+-   [countries.json](https://raw.githubusercontent.com/DealPete/forceDirected/master/countries.json)
 
 ## Build
 
-* `npm start` - start webpack-dev-server
-* `npm build` - build for production
+-   `npm start` - start webpack-dev-server
+-   `npm build` - build for production
 
 ## License
 
--- src/js/index.js
+++ src/js/index.js
@@ -1,12 +1,26 @@
-/* eslint-disable no-param-reassign,import/no-dynamic-require,global-require,no-return-assign */
-import "../css/index.css";
+/* eslint-disable no-param-reassign */
+
+import {
+  drag,
+  event,
+  forceCenter,
+  forceCollide,
+  forceLink,
+  forceManyBody,
+  forceSimulation,
+  select,
+  zoom,
+  selectAll
+} from "d3";
+
+import "../css/index.pcss";
 import dataset from "./countries.json";
 
 const req = require.context("../assets", false, /.png$/);
 const flags = {};
 req.keys().forEach(key => {
-    // extract filename, e.g "./us.png" => "us"
-    flags[/[^./]+(?=\.png$)/.exec(key)[0]] = req(key);
+  // extract filename: "./us.png" => "us"
+  flags[/[^./]+(?=\.png$)/.exec(key)[0]] = req(key);
 });
 
 const w = 1000;
@@ -14,157 +28,184 @@ const h = 500;
 const flagSize = 32;
 
 // Initialize a simple force layout, using the nodes and edges in dataset
-const simulation = d3
-    .forceSimulation(dataset.nodes)
-    .force("charge", d3.forceManyBody().strength(-100))
-    .force("link", d3.forceLink(dataset.edges).distance(70))
-    .force(
-        "center",
-        d3
-            .forceCenter()
-            .x(w / 2)
-            .y(h / 2),
-    )
-    .force("collision", d3.forceCollide());
+const simulation = forceSimulation(dataset.nodes)
+  .force("charge", forceManyBody().strength(-100))
+  .force("link", forceLink(dataset.edges).distance(70))
+  .force(
+    "center",
+    forceCenter()
+      .x(w / 2)
+      .y(h / 2)
+  )
+  .force("collision", forceCollide());
 
-d3
-    .select("main")
-    .append("h1")
-    .text("National Contiguity");
+select("#chart")
+  .append("h1")
+  .classed("heading", true)
+  .text("National Contiguity");
 
-d3
-    .select("main")
-    .append("p")
-    .text("This chart shows countries which have common borders or have close proximity to each other.");
-d3
-    .select("main")
-    .append("small")
-    .text("To move click and drag. To zoom use the mouse wheel.");
+select("#chart")
+  .append("p")
+  .text(
+    "This chart shows countries which have common borders or have close proximity to each other."
+  );
+select("#chart")
+  .append("small")
+  .text("To move click and drag. To zoom use the mouse wheel.");
 
-const svg = d3
-    .select("main")
-    .append("div")
-    .append("svg")
-    .attr("width", w)
-    .attr("height", h);
+const svg = select("#chart")
+  .append("div")
+  .append("svg")
+  .attr("width", w)
+  .attr("height", h)
+  .classed("chart", true);
 
 svg
-    .append("rect")
-    .attr("width", "100%")
-    .attr("height", "100%")
-    .attr("fill", "#fff");
+  .append("rect")
+  .attr("width", "100%")
+  .attr("height", "100%")
+  .attr("fill", "#fff");
+
+const filterDef = svg.append("defs");
+const filter = filterDef
+  .append("filter")
+  .attr("id", "dropshadow")
+  .attr("height", "130%");
+filter
+  .append("feGaussianBlur")
+  .attr("in", "SourceAlpha")
+  .attr("stdDeviation", "1");
+filter
+  .append("feOffset")
+  .attr("dx", "1")
+  .attr("dy", "1")
+  .attr("result", "offsetblur");
+
+const feComponentTransfer = filter.append("feComponentTransfer");
+feComponentTransfer.append("feFuncA").attr("linear", "0.9"); // shadow opacity
+
+const filterMerge = filter.append("feMerge");
+filterMerge.append("feMergeNode");
+filterMerge.append("feMergeNode").attr("in", "SourceGraphic");
 
 const chart = svg.append("g");
 
-const drag = {
-    start(d) {
-        if (!d3.event.active) {
-            simulation.alphaTarget(0.3).restart();
-        }
-        d.fx = d.x;
-        d.fy = d.y;
-    },
-    during(d) {
-        d.fx = d3.event.x;
-        d.fy = d3.event.y;
-    },
-    end(d) {
-        if (!d3.event.active) {
-            simulation.alphaTarget(0);
-        }
-        d.fx = null;
-        d.fy = null;
-    },
+const dragging = {
+  start(d) {
+    if (!event.active) {
+      simulation.alphaTarget(0.3).restart();
+    }
+    d.fx = d.x;
+    d.fy = d.y;
+  },
+  during(d) {
+    d.fx = event.x;
+    d.fy = event.y;
+  },
+  end(d) {
+    if (!event.active) {
+      simulation.alphaTarget(0);
+    }
+    d.fx = null;
+    d.fy = null;
+  },
+  subject(d) {
+    return simulation.find(d.x, d.y);
+  }
 };
 
-// Create edges as lines
 const edges = chart
-    .selectAll("line")
-    .data(dataset.edges)
-    .enter()
-    .append("line")
-    .style("stroke", "#ccc")
-    .style("stroke-width", 1);
+  .selectAll("line")
+  .data(dataset.edges)
+  .enter()
+  .append("line")
+  .style("stroke", "#ccc")
+  .style("stroke-width", 1);
 
 function movingTooltip(x, y) {
-    d3
-        .select("#tooltip")
-        .attr("x", x + flagSize / 2)
-        .attr("y", y);
+  select("#tooltip")
+    .attr("x", x + flagSize / 2)
+    .attr("y", y);
 }
 
-function stoppingTooltip() {
-    d3.select("#tooltip").remove();
+function restingTooltip() {
+  select("#tooltip").remove();
 }
 
 function startingTooltip(x, y, country) {
-    chart
-        .append("text")
-        .attr("id", "tooltip")
-        .attr("x", x + flagSize / 2)
-        .attr("y", y)
-        .attr("text-anchor", "middle")
-        .attr("font-family", "sans-serif")
-        .attr("font-size", "11px")
-        .attr("font-weight", "bold")
-        .attr("fill", "black")
-        .text(country);
+  chart
+    .append("text")
+    .attr("id", "tooltip")
+    .attr("x", x + flagSize / 2)
+    .attr("y", y)
+    .attr("text-anchor", "middle")
+    .attr("font-family", "sans-serif")
+    .attr("font-size", "11px")
+    .attr("font-weight", "bold")
+    .attr("fill", "black")
+    .text(country);
 }
 
-// Create nodes as flags
 const nodes = chart
-    .selectAll("image")
-    .data(dataset.nodes)
-    .enter()
-    .append("svg:image")
-    .attr("xlink:href", ({ code }) => flags[code])
-    .attr("width", flagSize)
-    .attr("height", flagSize)
-    .on("mouseover", ({ x, y, country }) => {
-        startingTooltip(x, y, country);
-    })
-    .on("mousemove", ({ x, y }) => {
+  .selectAll("image")
+  .data(dataset.nodes)
+  .enter()
+  .append("svg:image")
+  .attr("xlink:href", ({ code }) => flags[code])
+  .attr("width", flagSize)
+  .attr("height", flagSize)
+  .on("mouseover", ({ x, y, country }) => {
+    startingTooltip(x, y, country);
+  })
+  .on("mousemove", ({ x, y }) => {
+    movingTooltip(x, y);
+  })
+  .on("mouseout", () => {
+    restingTooltip();
+  })
+  .call(
+    drag()
+      .subject(dragging.subject)
+      .on("start", d => {
+        dragging.start(d);
+      })
+      .on("drag", d => {
+        dragging.during(d);
+        const { x, y } = d;
         movingTooltip(x, y);
-    })
-    .on("mouseout", () => {
-        stoppingTooltip();
-    })
-    .call(
-        d3
-            .drag()
-            .on("start", d => {
-                drag.start(d);
-            })
-            .on("drag", d => {
-                drag.during(d);
-                const { x, y } = d;
-                movingTooltip(x, y);
-            })
-            .on("end", d => {
-                drag.end(d);
-                stoppingTooltip();
-            }),
-    );
+      })
+      .on("end", d => {
+        dragging.end(d);
+        restingTooltip();
+      })
+  );
+
+selectAll("#chart image").style("filter", "url(#dropshadow)");
 
 function zoomActions() {
-    chart.attr("transform", d3.event.transform);
+  chart.attr("transform", event.transform);
 }
 
-const zoomHandler = d3
-    .zoom()
-    .scaleExtent([0.2, 2.0])
-    .translateExtent([[-w * 3, -h * 3], [w * 3, h * 3]])
-    .on("zoom", zoomActions);
+const zoomHandler = zoom()
+  .scaleExtent([0.2, 2.0])
+  .translateExtent([[-w * 3, -h * 3], [w * 3, h * 3]])
+  .on("zoom", zoomActions);
 
 zoomHandler(svg);
 
 simulation.on("tick", () => {
-    nodes
-        .attr("x", d => (d.x = Math.max(-w * 3 + flagSize, Math.min(w * 3 - flagSize, d.x))))
-        .attr("y", d => (d.y = Math.max(-h * 3 + flagSize, Math.min(h * 3 - flagSize, d.y))));
-    edges
-        .attr("x1", d => d.source.x + flagSize / 2)
-        .attr("y1", d => d.source.y + flagSize / 2)
-        .attr("x2", d => d.target.x + flagSize / 2)
-        .attr("y2", d => d.target.y + flagSize / 2);
+  nodes
+    .attr("x", d => {
+      d.x = Math.max(-w * 3 + flagSize, Math.min(w * 3 - flagSize, d.x));
+      return d.x;
+    })
+    .attr("y", d => {
+      d.y = Math.max(-h * 3 + flagSize, Math.min(h * 3 - flagSize, d.y));
+      return d.y;
+    });
+  edges
+    .attr("x1", d => d.source.x + flagSize / 2)
+    .attr("y1", d => d.source.y + flagSize / 2)
+    .attr("x2", d => d.target.x + flagSize / 2)
+    .attr("y2", d => d.target.y + flagSize / 2);
 });
